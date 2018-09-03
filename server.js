cd4ad456516453c72bc6a5f9f06410933fe37a59
Nikita Jain <nikita97.jain@gmail.com>
2018-09-04 00:41:00 +0530

    Add project page

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.get('/about',(req,res) => {
 });
 });
 
+app.get('/projects',(req,res)=>{
+   res.render('projects.hbs',{
+       pageTitle:'Projects page'
+})
+});
+
 // /bad
 app.get('/bad',(req,res) => {
     res.send({
